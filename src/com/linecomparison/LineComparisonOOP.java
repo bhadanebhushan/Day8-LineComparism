38fce0b0e79602ceeab4e32e170cffc64673c84d
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-18 17:27:36 +0530

    Line comparison line length logic

--- src/com/linecomparison/LineComparisonOOP.java
+++ src/com/linecomparison/LineComparisonOOP.java
@@ -2,71 +2,50 @@ package com.linecomparison;
 
 import java.util.Scanner;
 
-class Coordinates {
-	Scanner sc = new Scanner(System.in);
-	// instance variables
-	static double x1, x2, y1, y2, x3, x4, y3, y4;
-	static int distance1, distance2;
+class CoOrdinates {
+	public double x;		// co-ordinate x
+	public double y;		// co-ordinate y
+	public double length;
 
-	// Computation for point1 & point2
-	public void point1() {
-		System.out.println("enter x1 & y1 values of first point");
-		x1 = sc.nextDouble();
-		y1 = sc.nextDouble();
+	public double getX() {
+		return x;
 	}
 
-	public void point2() {
-		System.out.println("enter x2 & y2 values of second point");
-		x2 = sc.nextDouble();
-		y2 = sc.nextDouble();
+	public void setX(double x) {
+		this.x = x;
 	}
 
-	public void distanceBetweenPoint1Point2() {
-		distance1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		System.out.println("Distance between two points = " + distance1);
+	public double getY() {
+		return y;
 	}
 
-	// Computation for point3 & point4
-	public void point3() {
-		System.out.println("enter x3 & y3 values of third point");
-		x3 = sc.nextDouble();
-		y3 = sc.nextDouble();
-	}
-
-	public void point4() {
-		System.out.println("enter x2 & y2 values of second point");
-		x4 = sc.nextDouble();
-		y4 = sc.nextDouble();
-	}
-
-	public void distanceBetweenPoint3Point4() {
-		distance2 = (int) Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
-		System.out.println("Distance between two points = " + distance2);
-	}
-
-	// checking for line equality
-	public void CompareLinesUsingEqualToMethod() {
-		String str1 = Double.toString(distance1);
-		String str2 = Double.toString(distance2);
-
-		if (str1.equals(str2) == true)
-			System.out.println("Both lines are equal");
-		else
-			System.out.println("Not both lines are equal");
+	public void setY(double y) {
+		this.y = y;
 	}
 }
 
 public class LineComparisonOOP {
-	public static void main(String[] args) {
-		Coordinates coordinate = new Coordinates();
-		// Base class method calling
-		coordinate.point1();
-		coordinate.point2();
-		coordinate.distanceBetweenPoint1Point2();
-		coordinate.point3();
-		coordinate.point4();
-		coordinate.distanceBetweenPoint3Point4();
-		coordinate.CompareLinesUsingEqualToMethod();
-	}
+	public static double lineLength() {
+		CoOrdinates co = new CoOrdinates();
+		try (Scanner scanner = new Scanner(System.in)) {
+			co.setX((scanner.nextInt()));
+			double x1 = co.getX();
+			co.setX((scanner.nextInt()));
+			double x2 = co.getX();
+			co.setY((scanner.nextInt()));
+			double y1 = co.getY();
+			co.setY((scanner.nextInt()));
+			double y2 = co.getY();
+			double x = Math.pow((x1 - x2), 2);
+			double y = Math.pow((y1 - y2), 2);
+			double length = Math.sqrt(x + y);
 
+			return length;
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Using OOP's Concept" + "\nEnter the CoOrdinates: ");
+		double lineLength = lineLength();
+		System.out.println("Length of line is " + lineLength);
+	}
 }
