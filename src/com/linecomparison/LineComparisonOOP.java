a333d5c28277eb80579334e06db65457fef2a8e3
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-18 16:49:04 +0530

    Welcome

---
+++ src/com/linecomparison/LineComparisonOOP.java
@@ -0,0 +1,7 @@
+package com.linecomparison;
+
+public class LineComparisonOOP {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Using OOP's Concept");
+	}
+}
